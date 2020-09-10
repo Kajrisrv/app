ada8c7a854f18047a466053464f216e1e5486f3a
Kajrisrv <70900484+Kajrisrv@users.noreply.github.com>
2020-09-11 02:09:34 +0530

    Add files via upload

---
+++ hello.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export const helloHandler: APIGatewayProxyHandler = async (event:APIGatewayProxyEvent, context:Context) => {
+    let response:APIGatewayProxyResult;
+    response = {
+        body: "Hello World!",
+        statusCode: 200
+    }
+
+    return response;
+};
